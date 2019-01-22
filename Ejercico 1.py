d664ec7ad9644764f0d4ded2d6e0d173ffb426d2
Sofia-Tv <46934834+Sofia-Tv@users.noreply.github.com>
2019-01-22 11:05:18 -0600

    Sofia Trujillo Vargas Grupo 03

---
+++ Ejercico 1.py
@@ -0,0 +1,5 @@
+#Autor: Sofía Trujillo V
+d=int(input("Teclea la distancia de tu viaje en kms enteros: "))
+t=int(input("Teclea el tiempo de tu viaje en hrs enteras: "))
+velocidad=d/t
+print("Tu velocidad es igual a : ", velocidad)
